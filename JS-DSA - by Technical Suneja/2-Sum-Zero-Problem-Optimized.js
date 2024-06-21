512cd99a9a6caab521bf255bf4029e9111fe389a
Vishwas Bhoir <CoderVishwas@gmail.com>
2024-06-21 21:16:44 +0530

    Sortten the solution for Sum zero problem

--- JS-DSA - by Technical Suneja/2-Sum-Zero-Problem-Optimized.js
+++ JS-DSA - by Technical Suneja/2-Sum-Zero-Problem-Optimized.js
@@ -2,25 +2,17 @@ let input = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
 
 // This is optimized solution for problem 1.
 const sumZeroOptimized = function (array) {
-    const pairs = [];
-    let left = 0;
-    let right = array.length - 1;
-    while (left < right) {
-        let sum = array[left] + array[right];
-        if (sum === 0) {
-            pairs.push([array[left], array[right]]);
-            console.log(left, right);
-            left++;
-            right--;
-        } else if (sum > 0) {
-            console.log(left, right);
-            right--;
-        } else {
-            console.log(left, right);
-            left++;
-        }
-    }
-    return pairs;
+	let left = 0;
+	let right = array.length - 1;
+
+	while (left < right) {
+		let sum = array[left] + array[right];
+		if (sum === 0) return [left, right];
+		if (sum > 0) right--;
+		if (sum < 0) left++;
+	}
+
+	return "No pair with sum 0";
 };
 
 console.log(sumZeroOptimized(input));
